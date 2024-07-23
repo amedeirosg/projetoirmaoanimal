36027304c5ceed454c46fc52a8324e17f2724711
amedeirosg <gomesandremedeiros@gmail.com>
2024-07-23 02:30:56 -0300

    finish project

--- src/app/Components/Actions/actions.tsx
+++ src/app/Components/Actions/actions.tsx
@@ -15,6 +15,8 @@ import helpDogTwo from "../../../assets/helpdogs2.jpg";
 import adoption1 from "../../../assets/adocao1.jpg";
 import adoption2 from "../../../assets/adocao2.jpg";
 import adoption3 from "../../../assets/adocao3.jpg";
+import Link from "next/link";
+
 export default function Actions() {
   const sliderImgs = [
     {
@@ -40,34 +42,156 @@ export default function Actions() {
       <div className="ActionsSlider">
         <div className="ActionsCatCaoCourse">
           <h3>Nosso concurso de aumigos :)</h3>
+          <span>
+            Concurso realizado pelo nosso{" "}
+            <Link
+              href="https://www.instagram.com/projetoirmaoanimal/"
+              style={{
+                all: "unset",
+                cursor: "pointer",
+                textDecoration: "underline",
+              }}
+            >
+              <span>Instagram</span>
+            </Link>
+          </span>
           <div className="ActionsCatCaoCourseImg">
-            <Image src={votes} alt="aumigos" width={300} height={300} />
-            <Image src={charllote} alt="aumigos" width={300} height={300} />
-            <Image src={barto} alt="aumigos" width={300} height={300} />
+            <Image
+              src={votes}
+              alt="aumigos"
+              className="ImgsStyle"
+              width={300}
+              height={500}
+            />
+            <Image
+              src={charllote}
+              alt="aumigos"
+              className="ImgsStyle"
+              width={300}
+              height={500}
+            />
+            <Image
+              src={barto}
+              alt="aumigos"
+              className="ImgsStyle"
+              width={300}
+              height={500}
+            />
           </div>
         </div>
         <div className="ActionsRaffle">
           <h3>Rifas realizadas</h3>
+          <span>
+            Rifas realizadas pelo nosso{" "}
+            <Link
+              href="https://www.instagram.com/projetoirmaoanimal/"
+              style={{
+                all: "unset",
+                cursor: "pointer",
+                textDecoration: "underline",
+              }}
+            >
+              <span>Instagram</span>
+            </Link>
+          </span>
           <div className="ActionsRaffleImgs">
-            <Image src={raffleOne} alt="aumigos" width={300} height={300} />
-            <Image src={raffleTwo} alt="aumigos" width={300} height={300} />
+            <Image
+              src={raffleOne}
+              alt="aumigos"
+              className="ImgsStyle"
+              width={300}
+              height={500}
+            />
+            <Image
+              src={raffleTwo}
+              alt="aumigos"
+              className="ImgsStyle"
+              width={300}
+              height={500}
+            />
           </div>
         </div>
         <div className="ActionsHelped">
           <h3>Animais resgatados</h3>
           <div className="ActionsRaffleImgs">
-            <Image src={helpDogBefore} alt="aumigos" width={300} height={300} />
-            <Image src={helpDogAfter} alt="aumigos" width={300} height={300} />
-            <Image src={helpDogOne} alt="aumigos" width={300} height={300} />
-            <Image src={helpDogTwo} alt="aumigos" width={300} height={300} />
+            <div className="ActionsInfo">
+              <Image
+                src={helpDogBefore}
+                alt="aumigos"
+                width={300}
+                height={500}
+                className="ImgsStyle"
+              />
+              <span>Caso cachorro tal... (Antes)</span>
+            </div>
+            <div className="ActionsInfo">
+              <Image
+                src={helpDogAfter}
+                alt="aumigos"
+                className="ImgsStyle"
+                width={300}
+                height={500}
+              />
+              <span>Caso cachorro tal... (Depois)</span>
+            </div>
+
+            <div className="ActionsInfo">
+              <Image
+                src={helpDogOne}
+                alt="aumigos"
+                className="ImgsStyle"
+                width={300}
+                height={500}
+              />
+              <span>Filhotes tal...</span>
+            </div>
+            <div className="ActionsInfo">
+              <Image
+                src={helpDogTwo}
+                alt="aumigos"
+                className="ImgsStyle"
+                width={300}
+                height={500}
+              />
+              <span>Filhotes tal...</span>
+            </div>
           </div>
         </div>
         <div className="ActionsAdopted">
           <h3>Adoções</h3>
           <div className="ActionsAdoptedImgs">
-            <Image src={adoption1} alt="aumigos" width={300} height={300} />
-            <Image src={adoption2} alt="aumigos" width={300} height={300} />
-            <Image src={adoption3} alt="aumigos" width={300} height={300} />
+            <div className="ActionsInfo">
+              <Image
+                src={adoption1}
+                alt="aumigos"
+                className="ImgsStyle"
+                width={300}
+                height={500}
+              />
+              <span>Adoção feita na nossa feira em Pedro Leopoldo 🥰</span>
+            </div>
+
+            <div className="ActionsInfo">
+              <Image
+                src={adoption2}
+                alt="aumigos"
+                className="ImgsStyle"
+                width={300}
+                height={500}
+              />
+              <span>Adoção feita na nossa feira em Pedro Leopoldo 🥰</span>
+            </div>
+
+            <div className="ActionsInfo">
+              <Image
+                src={adoption3}
+                alt="aumigos"
+                className="ImgsStyle"
+                width={300}
+                height={500}
+              />
+              <span>Adoção feita por uma de nossas voluntárias 🥰</span>
+            </div>
           </div>
         </div>
       </div>
--- src/app/Components/Help/help.tsx
+++ src/app/Components/Help/help.tsx
@@ -1,3 +1,4 @@
+"use client";
 import "./Help.css";
 import Image from "next/image";
 import cats from "../../../assets/cats.png";
@@ -5,11 +6,21 @@ import dog from "../../../assets/dog.png";
 import dogs from "../../../assets/dogs.png";
 import house from "../../../assets/house.png";
 export default function Help() {
+  const sendMsg = () => {
+    var phoneNumber = "5551987654321"; // Substitua pelo número desejado, com o código do país e da área
+    var message = "Olá! Gostaria de mais informações."; // Mensagem opcional
+    var whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
+      message
+    )}`;
+
+    window.open(whatsappLink, "_blank");
+  };
+
   return (
     <div className="HelpContainer" id="help">
       <h2>Como ajudar?</h2>
       <div className="HelpArea group">
-        <div className="HelpTags item">
+        <div className="HelpTags item" onClick={sendMsg}>
           <Image src={cats} alt="imagem" />
           <h2>Seja um parceiro do projeto</h2>
           <span>
@@ -17,12 +28,12 @@ export default function Help() {
             conosco!
           </span>
         </div>
-        <div className="HelpTags item">
+        <div className="HelpTags item " onClick={sendMsg}>
           <Image src={dog} alt="imagem" />
           <h2>Seja voluntário!</h2>
           <span>Ajude com tarefas importantes para o nosso projeto!</span>
         </div>
-        <div className="HelpTags item">
+        <div className="HelpTags item" onClick={sendMsg}>
           <Image src={dogs} alt="imagem" />
           <h2>Ofereça lar temporário</h2>
           <span>
@@ -30,7 +41,7 @@ export default function Help() {
             incapacitados
           </span>
         </div>
-        <div className="HelpTags item">
+        <div className="HelpTags item" onClick={sendMsg}>
           <Image src={house} alt="imagem" />
           <h2>Apadrinhe um animal</h2>
           <span>
